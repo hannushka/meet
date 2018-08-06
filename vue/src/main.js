3214a182f97ec48cba5957d313a98e063b3f7a7c
Hannushka <lindbladhannah@gmail.com>
2018-08-06 14:36:40 +0200

    Initial setup with Vue
    Add fontawesome

---
+++ vue/src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserFriends, faComments, faCalendar, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserFriends)
+library.add(faComments)
+library.add(faCalendar)
+library.add(faPlus)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
