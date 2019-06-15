ca4cc65bde970a0bab1dcd66b8582f7909e80f27
Hannah <lindbladhannah@gmail.com>
2019-06-15 18:22:39 +0200

    Setup routing

--- meet-frontend/src/index.js
+++ meet-frontend/src/index.js
@@ -1,4 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.js";
-ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './styles.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World! :)</h1>
+    </div>
+  );
+}
+
+function NoMatch(_) {
+  return (
+    <div>
+      <h3>Nothing to see here :(</h3>
+    </div>
+  );
+}
+
+const Root = () => {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={App} />
+          <Route path="" component={NoMatch} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<Root />, rootElement);
